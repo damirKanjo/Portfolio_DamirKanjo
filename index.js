2e16cfd206c620499f4069c637e3f71079495576
damirKanjo <95128158+damirKanjo@users.noreply.github.com>
2025-01-15 18:15:00 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,92 +1,55 @@
-window.onload = hideIcons(0);
-window.onload = mobDesk();
-window.onresize = hideIcons(0);
 window.onresize = width;
-
-
-function mobDesk(){
-    let details = navigator.userAgent;
-      
-    /* Creating a regular expression
-      containing some mobile devices keywords
-      to search it in details string*/
-    let regexp = /android|iphone|kindle|ipad/i;
-      
-    /* Using test() method to search regexp in details
-      it returns boolean value*/
-    let isMobileDevice = regexp.test(details);
-      
-    if (isMobileDevice) {
-        document.querySelector("#message").style.paddingTop = "150px";
-    } else {
-        document.querySelector("#message").style.paddingTop = "0px";
-    }
-}
 
 function width() {
     let a = window.innerWidth;
     return a;
 }
 
-function showIcons(y) {
-    if (width() >= 480) {
-        document.querySelector(".tel").style.left = "5px";
-        document.querySelector(".mail").style.left = "10px";
-        document.querySelector(".viber").style.left = "15px";
-        document.querySelector(".linkedin").style.left = "20px";
-        hideIcons(y);
-    }
+
+function showAbout(){
+  clearScreen();
+  document.querySelector('.about').style.display = 'block';
+  document.querySelector('.about').style.animation = 'moving-top 1.75s linear forwards';
+  document.querySelector('.img').style.animation = 'img-show 2.25s linear forwards';
 }
 
-function hideIcons(x) {
-    setTimeout(() => {
-        document.querySelector(".tel").style.left = "-100px";
-        document.querySelector(".mail").style.left = "-150px";
-        document.querySelector(".viber").style.left = "-200px";
-        document.querySelector(".linkedin").style.left = "-250px";
-    }, x);
+function showEducation(){
+  clearScreen();
+  document.querySelector('.education').style.display = 'block';
+  document.querySelector('.timeline').style.animation = 'slide-in2 2s linear forwards';
+  document.querySelector('.ev1').style.animation = 'slide-in 1.75s linear forwards';
+  document.querySelector('.ev2').style.animation = 'slide-in1 1.75s linear forwards';
+  document.querySelector('.ev3').style.animation = 'slide-in 1.75s linear forwards';
 }
 
-
-let about = document.querySelector("#about");
-let edu = document.querySelector("#education");
-let exp = document.querySelector("#experience");
-let skill = document.querySelector("#skills");
-
-function myFunction(x) {
-    let a = x;
-    if (a == 1) {
-        clear();
-        document.querySelector('.section').appendChild(about);
-        about.style.display = 'block';
-    }
-    if (a == 2) {
-        clear();
-        document.querySelector('.section').appendChild(edu);
-        edu.style.display = 'block';
-    }
-    if (a == 3) {
-        clear();
-        document.querySelector('.section').appendChild(exp);
-        exp.style.display = 'block';
-    }
-    if (a == 4) {
-        clear();
-        document.querySelector('.section').appendChild(skill);
-        skill.style.display = 'block';
-    }
+function showExperience() {
+  clearScreen();
+  document.querySelector('.experience').style.display = 'flex';
+  document.querySelector('.experience').style.animation = 'apear 2.5s ease-in-out forwards';
 }
 
+function showSkills() {
+  clearScreen();
+  document.querySelector('.skills').style.display = 'block';
+  let x = document.querySelectorAll(".move");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.animation = 'slide-bottom 3s ease-in-out forwards';
+  }
+}
 
-const buttons = document.querySelectorAll('.close');
-buttons.forEach(button => {
-    button.addEventListener('click', clear);
+function clearScreen(){
+  document.querySelector('.typewriter').style.display = 'none';
+  document.querySelector('.about').style.display = 'none';
+  document.querySelector('.education').style.display = 'none';
+  document.querySelector('.experience').style.display = 'none';
+  document.querySelector('.skills').style.display = 'none';
+}
+
+document.querySelector(".navbar-toggler").addEventListener('click', () => {
+  document.querySelector('.typewriter').style.display = 'none';
+  if(document.querySelector('.navbar-collapse').style.height >= '10px'){
+    document.querySelector('.navbar').style.marginBottom = '320px';
+  } else{
+    document.querySelector('.navbar').style.marginBottom = '0px';
+  }
 });
-
-
-function clear() {
-    about.style.display = 'none'; 
-    edu.style.display = 'none'; 
-    exp.style.display = 'none'; 
-    skill.style.display = 'none';    
-}
